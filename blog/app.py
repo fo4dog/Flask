1e23d98f838b644087e4a175ede6174cb574edfa
fo4dog <7249901@gmail.com>
2023-04-09 22:05:40 +0300

    HW_lesson_2

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+from blog.article.views import article
+from blog.user.views import user
 
 
-@app.route("/")
-def index():
-    return "Hello web!"
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(user)
+    app.register_blueprint(article)
