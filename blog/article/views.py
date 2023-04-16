6ebddec559200ee306fff0c7ff351fbf76dce06c
fo4dog <7249901@gmail.com>
2023-04-16 20:07:08 +0300

    HW_lesson_3

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 
 article = Blueprint("article", __name__, url_prefix="/articles", static_folder="../static")
@@ -8,21 +9,21 @@ ARTICLES = {
     1: {"title": "title 1",
         "text": "text 1",
         "author": {
-            "name": "Alice",
+            "email": "name@email.com",
             "pk": 1,
         }
         },
     2: {"title": "title 2",
         "text": "text 2",
         "author": {
-            "name": "Jon",
+            "email": "name@email.com",
             "pk": 2,
         }
         },
     3: {"title": "title 3",
         "text": "text 3",
         "author": {
-            "name": "Alice",
+            "email": "name@example.com",
             "pk": 1,
         }
         },
@@ -30,11 +31,13 @@ ARTICLES = {
 
 
 @article.route("/")
+@login_required
 def article_list():
     return render_template('articles/list.html', articles=ARTICLES)
 
 
 @article.route("/<int:pk>")
+@login_required
 def get_article(pk: int):
     try:
         article_name = ARTICLES[pk]
